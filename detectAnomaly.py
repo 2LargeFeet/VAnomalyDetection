564ee6d3395e0cebdf331dd5611ba3384dc36477
2LargeFeet <michael.gombos@gmail.com>
2025-03-03 11:15:28 -0500

    added Teams notifications for anomaly detection

--- detectAnomaly.py
+++ detectAnomaly.py
@@ -1,13 +1,45 @@
 from getToken import APIClient
 from datetime import datetime, timedelta, timezone
 from collections import defaultdict
+import requests
 import sys
 import os
 
 ## Pass API key from terminal or environmental variable
 API_KEY = os.getenv("API_KEY")
+TEAMS_WEBHOOK = os.getenv("TEAMS_WEBHOOK")
 AUTH_URL = "https://api.verkada.com/token"
 BASE_URL = "https://api.verkada.com/events/v1/"
+
+## Manage notifications
+def sendTeamsNotification(notification):
+    payload = {
+        "type": "message",
+        "attachments": [
+            {
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "content": {
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "type": "AdaptiveCard",
+                    "version": "1.4",
+                    "body": [
+                        {
+                            "type": "TextBlock",
+                            "size": "Large",
+                            "weight": "Bolder",
+                            "text": "New Access Alert"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "text": notification,
+                            "wrap": True
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+    response = requests.post(TEAMS_WEBHOOK, json=payload)
 
 ## Manage time
 dt = datetime.now(timezone.utc)
@@ -28,5 +60,5 @@ for event in eventList:
 
 for device in deviceList:
     if deviceList[device] > 4:
-            print(f"Too many auth failures on {device}")
-
+        notification = f"Too many auth failures on {device}"
+        sendTeamsNotification(notification)
